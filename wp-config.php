dc958839d84fdcc22712a2457cba057b40627b00
aalston004 <aalston004@gmail.com>
2015-06-16 20:06:55 -0400

    - Development added

--- wp-config.php
+++ wp-config.php
@@ -78,3 +78,6 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+//Since not using Bedrock
+define('WP_ENV', 'development');
